0930b1222ed5f012ff78d36f1d18259211cc2079
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-01-25 12:32:07 +0530

    output box sttyling

--- src/ColorOutput.js
+++ src/ColorOutput.js
@@ -3,27 +3,39 @@ import React from "react";
 const ColorOutput = ({ color, hexValue }) => {
   return (
     <div className="color-output">
-      <input
-        type="text"
-        name="color"
-        id="color"
-        value={color ? color : ""}
-      />
-      <button className="color copy-btn">
-        Copy
-      </button>
+      <label
+        style={{
+          display: "block",
+          position: "relative",
+          right: "80px",
+          marginBottom: '5px'
+        }}
+        htmlFor="color"
+      >
+        Color Name
+      </label>
 
-      <input
-        type="text"
-        name="hex"
-        id="hex"
-        value={hexValue ? hexValue : ""}
-        />
+      <input type="text" name="color" id="color" value={color ? color : ""} />
+      <button className="color copy-btn">Copy</button>
+      <label
+        style={{
+          display: "block",
+          position: "relative",
+          right: "80px",
+          marginBottom: '5px'
+        }}
+        htmlFor="color"
+      >
+        Hex Value
+      </label>
 
-        <button className="hex copy-btn">
-          Copy
-        </button>
+      <input 
+        type="text" 
+        name="hex" 
+        id="hex" 
+        value={hexValue ? hexValue : ""} />
 
+      <button className="hex copy-btn">Copy</button>
     </div>
   );
 };
